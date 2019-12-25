08f1acf7d7ee4ea755fadf635dc1ad0d28ba2841
Will Cooper Jr <wcooperjr46@gmail.com>
2019-12-24 21:00:12 -0500

    Updates

---
+++ precache-manifest.90a687e2fe77fcf09a8353041747d177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e2804b854320bad0cb0a354fb3734f",
+    "url": "/coop-react-app/index.html"
+  },
+  {
+    "revision": "8d804c63a77269088903",
+    "url": "/coop-react-app/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "e15951e3330a79b6603c",
+    "url": "/coop-react-app/static/js/2.966d673a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/coop-react-app/static/js/2.966d673a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d804c63a77269088903",
+    "url": "/coop-react-app/static/js/main.e78af6a2.chunk.js"
+  },
+  {
+    "revision": "47bb79a8970f306f85ad",
+    "url": "/coop-react-app/static/js/runtime-main.b9245de1.js"
+  }
+]);
